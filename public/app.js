95fcb755075eed6c5c5dc999a6c92bc58733bc34
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-09-07 17:29:03 -0400

    simplified solution, removes ui router and welcome html

--- public/app.js
+++ public/app.js
@@ -2,24 +2,9 @@
 
 (function(){
   angular
-  .module("notesapp", [
-    "ui.router",
-    "ngResource",
-  ])
-  .config([
-    "$stateProvider",
-    Router
-  ])
+  .module("notesapp", [])
   .controller("notes_controller", NotesController);
 
-  function Router($stateProvider, $locationProvider){
-    $stateProvider
-    .state("welcome", {
-      url: "/",
-      templateUrl: "welcome.html",
-      controller: "notes_controller"
-    })
-  }
   function NotesController() {
     var vm = this;
     vm.notes = [
@@ -43,21 +28,3 @@
     };
   }
 })();
-
-
-/*
-[
-  {
-    name: "First Note",
-    content: "do something"
-  },
-  {
-    name: "Second Note",
-    content: "do another thing"
-  },
-  {
-    name: "Third Note",
-    content: "third thing"
-  }
-];
-*/
