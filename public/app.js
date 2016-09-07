1200e6c5489e4b2a65cadebe71d25dce02b41154
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-09-07 10:19:57 -0400

    notes showing up in array style

--- public/app.js
+++ public/app.js
@@ -16,24 +16,31 @@
     $stateProvider
     .state("welcome", {
       url: "/",
-      templateUrl: "welcome.html"
+      templateUrl: "welcome.html",
+      controller: "notes_controller"
     })
   }
   function NotesController() {
     var vm = this;
-    vm.notes = [
-      {
-        name: "First Note",
-        content: "do something"
-      },
-      {
-        name: "Second Note",
-        content: "do another thing"
-      },
-      {
-        name: "Third Note",
-        content: "third thing"
-      }
-    ];
+    vm.notes = ["one","two","three"];
+    console.log(vm.notes);
   }
 })();
+
+
+/*
+[
+  {
+    name: "First Note",
+    content: "do something"
+  },
+  {
+    name: "Second Note",
+    content: "do another thing"
+  },
+  {
+    name: "Third Note",
+    content: "third thing"
+  }
+];
+*/
